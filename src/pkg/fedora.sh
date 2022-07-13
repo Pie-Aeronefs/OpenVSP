e76f8b20363b38669b76daa723904fcb0cbefaeb
Jeff Allen <jallen@pieaeronefs.ch>
2022-07-13 10:51:10 +0200

    First try at RPM build.
    Signed-off-by: Jeff Allen <jallen@pieaeronefs.ch>

---
+++ src/pkg/fedora.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# These are the instructions we would put in the
+# Githib workflow file, except that we want to run
+# them from within a Docker container, so it is
+# more useful to have them here in fedora.sh.
+
+set -e
+
+env
+
+mkdir -p build buildlibs artifacts
+
+cd $GITHUB_WORKSPACE/buildlibs
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true -DVSP_USE_SYSTEM_GLM=true \
+    -DVSP_USE_SYSTEM_GLEW=true -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries
+make -j
+        
+cd $GITHUB_WORKSPACE/build
+cmake -DCMAKE_BUILD_TYPE=Release -DVSP_CPACK_GEN=DEB -DVSP_LIBRARY_PATH=$GITHUB_WORKSPACE/buildlibs ../src
+make -j package
+
+cp *.rpm $GITHUB_WORKSPACE/artifacts
+ls -l $GITHUB_WORKSPACE/artifacts
+echo "RPM CREATION SUCCESSFUL"
