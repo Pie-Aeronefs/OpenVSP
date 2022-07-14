661e5c6f268e2f6976151ef8b60510389f8be71f
Jeff Allen <jallen@pieaeronefs.ch>
2022-07-14 12:42:04 +0200

    Try with more shorter steps.
    Signed-off-by: Jeff Allen <jallen@pieaeronefs.ch>

--- src/pkg/fedora.sh
+++ src/pkg/fedora.sh
@@ -5,29 +5,42 @@
 # them from within a Docker container, so it is
 # more useful to have them here in fedora.sh.
 
-set -e -x
-
-dnf install -y cmake libxml2-devel fltk-fluid fltk-devel g++ \
-        openjpeg-devel glm-devel \
-        cminpack-devel glew-devel swig doxygen graphviz texlive-scheme-basic \
-        python3-devel
+set -e
 
 # docker shares the workspace directory into /workspace
 GITHUB_WORKSPACE=/workspace
 export GITHUB_WORKSPACE
-
 cd $GITHUB_WORKSPACE
 mkdir -p build buildlibs artifacts
 
-cd $GITHUB_WORKSPACE/buildlibs
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true -DVSP_USE_SYSTEM_GLM=true \
-    -DVSP_USE_SYSTEM_GLEW=true -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries
-make -j
-        
-cd $GITHUB_WORKSPACE/build
-cmake -DCMAKE_BUILD_TYPE=Release -DVSP_CPACK_GEN=DEB -DVSP_LIBRARY_PATH=$GITHUB_WORKSPACE/buildlibs ../src
-make -j package
+case $1 in
+install)
+    dnf install -y cmake libxml2-devel fltk-fluid fltk-devel g++ \
+            openjpeg-devel glm-devel \
+            cminpack-devel glew-devel swig doxygen graphviz texlive-scheme-basic \
+            python3-devel
+    ;;
+libs)
+    cd $GITHUB_WORKSPACE/buildlibs
+    cmake -DCMAKE_BUILD_TYPE=Release -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true -DVSP_USE_SYSTEM_GLM=true \
+        -DVSP_USE_SYSTEM_GLEW=true -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries
+    make -j
+    ;;
+exe)
+    cd $GITHUB_WORKSPACE/build
+    cmake -DCMAKE_BUILD_TYPE=Release -DVSP_CPACK_GEN=DEB -DVSP_LIBRARY_PATH=$GITHUB_WORKSPACE/buildlibs ../src
+    make -j
+    ;;
+rpm)
+    cd $GITHUB_WORKSPACE/build
+    cmake -DCMAKE_BUILD_TYPE=Release -DVSP_CPACK_GEN=DEB -DVSP_LIBRARY_PATH=$GITHUB_WORKSPACE/buildlibs ../src
+    make -j package
+    cp *.rpm $GITHUB_WORKSPACE/artifacts
+    ls -l $GITHUB_WORKSPACE/artifacts
+    echo "RPM CREATION SUCCESSFUL"
+    ;;
+*)
+    echo "unknown step $1"
+    exit 1
+esac
 
-cp *.rpm $GITHUB_WORKSPACE/artifacts
-ls -l $GITHUB_WORKSPACE/artifacts
-echo "RPM CREATION SUCCESSFUL"
